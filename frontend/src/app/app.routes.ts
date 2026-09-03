abbe6250f105b7a5022721307d775751b25d7974
Sakciiii <uttamsakshi386@gmail.com>
2026-09-04 00:22:11 +0530

    Configure frontend with React and Vite

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login.component';
-import { SignupComponent } from './features/auth/signup.component';
-export const routes: Routes = [
-  {path: 'login', component: LoginComponent}, {path: 'signup', component: SignupComponent}, {path: '', pathMatch: 'full', redirectTo: 'login'}, {path: '**', redirectTo: 'login'}
-];
